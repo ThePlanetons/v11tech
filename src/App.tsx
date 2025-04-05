e52739ea9f00c27f990df31162a271518b09d602
test <>
2025-04-05 18:04:17 +0530

    [main]css

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   
 
   return (
-    <><PricingSection></PricingSection><ShopsSection></ShopsSection></>
+    <><ShopsSection></ShopsSection><PricingSection></PricingSection></>
   
   )
 }
