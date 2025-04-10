1384512c32654ab7e4697e65a6c50d3e5592777e
test <>
2025-04-10 14:00:17 +0530

    [merge]css

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ import './App.css';
 import TestimonialCarousel from './carousel';
 import V11QRLanding from './V11tech';
 import HomePage from './Home';
+import TalkUs from './Talkus';
+import Footer from './Footer';
 // import HeroSection from './demo';
 //import { 
 function App() {
   
 
   return (
-    <><HomePage></HomePage><ShopsSection></ShopsSection><PricingSection></PricingSection><TestimonialCarousel/><V11QRLanding/></>
+    <><HomePage></HomePage><ShopsSection></ShopsSection><TalkUs></TalkUs><PricingSection></PricingSection><TestimonialCarousel/><V11QRLanding/><Footer></Footer></>
   
   
   )
