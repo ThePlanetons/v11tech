d32cb03e93a5529e833986c9960b3ebe64baa6b3
raveen2k <raveen119raveen@gmail.com>
2025-04-19 00:04:25 +0530

    [footer] changes

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -8,79 +8,82 @@ const Footer: React.FC = () => {
         {/* Main Footer Content */}
         <div className="flex flex-col md:flex-row justify-between items-center">
           {/* Logo on Left */}
-          <div className="mb-6 md:mb-0 pl-25 transform transition-transform hover:scale-105">
+          <div className="mb-6 md:mb-0 md:pl-4 lg:pl-8 transform transition-transform hover:scale-105 w-full md:w-auto flex justify-center md:justify-start">
             <div className="flex items-center">
-              <div className=" text-white pt-10">
+              <div className="text-white pt-4 md:pt-10">
                 <span className="font-bold text-lg"></span>
-            <img src="/assets/img/Logo_Last.png" alt="V11 logo" className="h-50 w-50 pb-10" />
-
+                <img src="/assets/img/Logo_Last.png" alt="V11 logo" className="h-16 w-80 md:h-20 pb-4 md:pb-10" />
                 <div className="text-xs"></div>
               </div>
             </div>
           </div>
 
           {/* Center - Follow and Social Links */}
-          <div className="mb-6 md:mb-0 text-center pl-5 pb-8">
+          <div className="mb-6 md:mb-0 text-center w-full md:w-auto">
             <p className="text-center font-medium mb-3">Follow V11 Tech®</p>
-            <div className="flex space-x-3 justify-center">
-              <a href="#" className=" rounded-full p-2 transform transition-all hover:scale-110 duration-200">
-                <Instagram size={35} className="" />
+            <div className="flex space-x-2 md:space-x-3 justify-center">
+              <a href="#" className="rounded-full p-1 md:p-2 transform transition-all hover:scale-110 duration-200">
+                <Instagram size={24} className="md:w-8 md:h-8" />
               </a>
-              <a href="#" className="rounded-full p-2 transform transition-all hover:scale-110 duration-200">
-                <Facebook size={35} className="" />
+              <a href="#" className="rounded-full p-1 md:p-2 transform transition-all hover:scale-110 duration-200">
+                <Facebook size={24} className="md:w-8 md:h-8" />
               </a>
-              <a href="#" className=" rounded-full p-2 transform transition-all hover:scale-110 duration-200">
-                <Linkedin size={35} className="" />
+              <a href="#" className="rounded-full p-1 md:p-2 transform transition-all hover:scale-110 duration-200">
+                <Linkedin size={24} className="md:w-8 md:h-8" />
               </a>
-              <a href="#" className=" rounded-full p-2 transform transition-all hover:scale-110 duration-200">
-                <Twitter size={35} className="" />
+              <a href="#" className="rounded-full p-1 md:p-2 transform transition-all hover:scale-110 duration-200">
+                <Twitter size={24} className="md:w-8 md:h-8" />
               </a>
-              <a href="#" className=" rounded-full p-2 transform transition-all hover:scale-110 duration-200">
-                <MessageCircle size={35} className="" />
+              <a href="#" className="rounded-full p-1 md:p-2 transform transition-all hover:scale-110 duration-200">
+                <MessageCircle size={24} className="md:w-8 md:h-8" />
               </a>
             </div>
           </div>
 
           {/* Right - App Download */}
-          <div className="text-center md:text-right">
-            <p className="font-medium pr-24 mb-3">Download our App</p>
-            <div className="flex flex-row space-y-2">
+          <div className="text-center md:text-right w-full md:w-auto">
+            <p className="font-medium mb-3 md:pr-6 lg:pr-24">Download our App</p>
+            <div className="flex flex-col sm:flex-row justify-center md:justify-end space-y-2 sm:space-y-0 sm:space-x-2">
               <a href="#" className="inline-block transform transition-transform hover:scale-105">
-                <img src="/assets/img/playstore.png" alt="Google Play" className="h-8 w-40" />
+                <img src="/assets/img/playstore.png" alt="Google Play" className="h-8 w-32 md:w-40" />
               </a>
               <a href="#" className="inline-block transform transition-transform hover:scale-105">
-                <img src="/assets/img/appstore.png" alt="App Store" className="h-7 w-40" />
+                <img src="/assets/img/appstore.png" alt="App Store" className="h-7 w-32 md:w-40" />
               </a>
             </div>
           </div>
         </div>
 
-        <div className="relative flex items-center justify-center pl-11 pb-8 max-w-full">
-          <p>V11 POS Software simplifies your business <br/>with smart, efficient, and modern solutions.</p>
-  {/* Centered Navigation */}
-  <nav className="absolute left-1/2 -translate-x-1/2 pr-6">
-    <ul className="flex flex-wrap justify-center space-x-4 md:space-x-8">
-      <li>
-        <a href="#" className="hover:underline transition-all duration-200">Home</a>
-      </li>
-      <li>
-        <a href="#" className="hover:underline transition-all duration-200">Terms and Condition</a>
-      </li>
-      <li>
-        <a href="#" className="hover:underline transition-all duration-200">Privacy Policy</a>
-      </li>
-      <li>
-        <a href="#" className="hover:underline transition-all duration-200">Contact Us</a>
-      </li>
-    </ul>
-  </nav>
+        {/* Middle section with tagline, navigation and payment */}
+        <div className="relative flex flex-col md:flex-row items-center justify-between mt-6 md:mt-0 pb-8">
+          {/* Tagline */}
+          <div className="text-center md:text-left mb-4 md:mb-0 md:pl-4 lg:pl-11 w-full md:w-1/3">
+            <p>V11 POS Software simplifies your business <br className="hidden sm:block"/>with smart, efficient, and modern solutions.</p>
+          </div>
+          
+          {/* Centered Navigation */}
+          <nav className="mb-4 md:mb-0 md:absolute md:left-1/2 md:-translate-x-1/2 w-full md:w-auto">
+            <ul className="flex flex-wrap justify-center space-x-4 md:space-x-8">
+              <li>
+                <a href="#" className="hover:underline transition-all duration-200">Home</a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline transition-all duration-200 text-xs sm:text-base">Terms and Condition</a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline transition-all duration-200">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline transition-all duration-200">Contact Us</a>
+              </li>
+            </ul>
+          </nav>
 
-  {/* Right-Aligned Payment Image */}
-  <div className="ml-auto justify-end pb-10">
-    <img src="/assets/img/payment.png" alt="Payment Methods" className="h-8 w-85" />
-  </div>
-</div>
-                  
+          {/* Right-Aligned Payment Image */}
+          <div className="md:ml-auto w-full md:w-1/3 flex justify-center md:justify-end md:pr-4 lg:pr-8">
+            <img src="/assets/img/payment.png" alt="Payment Methods" className="h-8 w-auto" />
+          </div>
+        </div>
 
         {/* Bottom Section */}
         <div className="flex flex-col md:flex-row border-t border-green-400 pt-4">
@@ -88,8 +91,6 @@ const Footer: React.FC = () => {
           <div className="mb-4 md:mb-0 text-sm flex justify-center w-full">
             <p>©2025 V11Tech. All rights reserved</p>
           </div>
-
-
         </div>
       </div>
     </footer>
